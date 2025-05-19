5493956ed08913e49b6149ca8f7d06b6e2a3fe0b
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-19 11:25:36 -0400

    add google maps integration

--- app/trip/page.tsx
+++ app/trip/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+import{
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,    
+} from "@vis.gl/react-google-maps";
+import './styles.css';
+
 const Trip = () =>{
+  console.log(process.env.NEXT_PUBLIC_MAPS_API_KEY);
   return(
-    <div></div>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY!}>
+      <div className="mapViewer">
+        <Map zoom={9} center={{lat:51.5072, lng:0.1276}}/>
+      </div>
+    </APIProvider>
+
   );
 }
 export default Trip;
