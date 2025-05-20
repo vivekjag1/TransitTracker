f57d5f842f1304203b6e94c80fb6d1402403d9e9
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-20 15:13:50 -0400

    add google places API, and update pathfinding to support user input

--- app/trip/page.tsx
+++ app/trip/page.tsx
@@ -6,25 +6,39 @@ import {
   Pin,
   InfoWindow, useMap, useMapsLibrary,
 } from "@vis.gl/react-google-maps";
-import './styles.css';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import {useEffect, useState} from "react";
-import ToolBar from "@/components/ToolBar";
+import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
-import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
+import './styles.css';
+import '../../components/PathfindingCard.css'
+import {useEffect, useState} from "react";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AssistantNavigationIcon from '@mui/icons-material/AssistantNavigation';
+
+
+
+import Script from "next/script";
+import {string} from "prop-types";
 const Trip = () =>{
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey:process.env.NEXT_PUBLIC_MAPS_API_KEY!,
+    libraries:["places"]
+  });
+  if (!isLoaded) return <div>Loading...</div>;
   console.log(process.env.NEXT_PUBLIC_MAPS_API_KEY);
   return(
     <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY!}>
       <div className="parentContainer">
-        <ToolBar/>
+
         <div className="mapViewer">
-        <Map defaultZoom={9} defaultCenter={{lat: 51.5072, lng: 0.1276}} fullscreenControl={false}/>
-        <Directions/>
+          <PathfindingCard/>
+
+          <Map defaultZoom={9} defaultCenter={{lat: 51.5072, lng: 0.1276}} fullscreenControl={false}/>
+        <PathfindingCard/>
         </div>
       </div>
 
@@ -33,13 +47,21 @@ const Trip = () =>{
   );
 }
 
-const Directions = () =>{
-  const map = useMap();//hook returns instance of a map and renders directions
-  const routesLibrary = useMapsLibrary("routes"); //loads routes
-  //state to store directions service
+
+
+
+
+const PathfindingCard = () =>{
+  const {ready, value, setValue, suggestions:{status, data}, clearSuggestions} = usePlacesAutocomplete({debounce:300});
+  const [startLocationTemp, setStartLocationTemp] = useState<string>('');
+  const [endLocationTemp, setEndLocationTemp] = useState<string>('');
+  const [start, setStart] = useState<string>();
+  const [end, setEnd] = useState<string>();
   const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
   const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
   const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
+  const map = useMap();//hook returns instance of a map and renders directions
+  const routesLibrary = useMapsLibrary("routes"); //loads routes
   useEffect(()=>{
     if(!routesLibrary || !map){
       return;
@@ -49,14 +71,17 @@ const Directions = () =>{
     setDirectionsRenderer(new routesLibrary.DirectionsRenderer({map}));
     console.log("directions service set");
   }, [routesLibrary, map]); //initialize services
-
   useEffect(() =>{
     if(!directionsRenderer || !directionsService){
       return;
     }
+    if(!start || !end ){
+      return;
+    }
+    console.log("ROUTES ARE", end);
     directionsService.route({
-      origin:"19 Bedford Pl, London WC1B 5JA, UK",
-      destination:"Ground floor Plimsoll Building, Handyside St, London N1C 4BQ, UK",
+      origin:start,
+      destination:end,
       travelMode: google.maps.TravelMode.TRANSIT,
       provideRouteAlternatives:true,
     }).then(res =>{
@@ -65,10 +90,36 @@ const Directions = () =>{
       console.log(routes);
 
     })
-  }, [directionsService, directionsRenderer]);
-  console.log(routes);
-  return null;
+  }, [start, end, directionsService, directionsRenderer]);
+
+
+  const handleSubmit = () =>{
+    console.log("TESTING", startLocationTemp, endLocationTemp);
+    setStart(startLocationTemp);
+    setEnd(endLocationTemp);
+  }
+
+
+  return(
+    <div className="pathfindingCardWrapper">
+      <Autocomplete onChange={(event, value) => setStartLocationTemp(value!.description)} filterOptions={(x)=>x} className="placeSelector" onInputChange={(event, value) => setValue(value)} renderInput={(params) => <TextField {...params} label="Origin"  onChange={(e) => setStartLocationTemp(e.target.value)}/>} options={data} getOptionLabel={(option) => typeof option === 'string'? option:option.description} />
+      <Autocomplete onChange={(event, value) => setEndLocationTemp(value!.description)} filterOptions={(x) => x} onInputChange={(event, value) => setValue(value)}  className = "placeSelector" renderInput={(params) => <TextField {...params} label="Destination" onChange={(e) => setEndLocationTemp(e.target.value)}/>} disablePortal options={data}  getOptionLabel={(option) => typeof option === 'string'? option:option.description}/>
+      <div className = "buttonContainer">
+        <Button className = "pathfindingButton" variant="contained" sx={{backgroundColor:'darkblue'}} onClick={handleSubmit}>
+          <div className = "buttonContent">
+            <AssistantNavigationIcon className = "buttonIcon"/>
+            <p>Navigate</p>
+          </div>
+
+        </Button>
+      </div>
+
+    </div>
+  )
 }
 
-
 export default Trip;
+
+
+
+
