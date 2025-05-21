70d0db8c1c6e88575e6a58d5d166dcdae3dacb25
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-20 21:22:50 -0400

    update pathfinding card styling

--- app/trip/page.tsx
+++ app/trip/page.tsx
@@ -18,6 +18,8 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import AssistantNavigationIcon from '@mui/icons-material/AssistantNavigation';
+import ShareLocationIcon from '@mui/icons-material/ShareLocation';
+import FlagIcon from '@mui/icons-material/Flag';
 const Trip = () =>{
   const {isLoaded} = useLoadScript({
     googleMapsApiKey:process.env.NEXT_PUBLIC_MAPS_API_KEY!,
@@ -54,6 +56,11 @@ const PathfindingCard = () =>{
   const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
   const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
   const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
+  const [routeIndex, setRouteIndex] = useState<number>(0);
+  const selected = routes[routeIndex];
+  const leg = selected?.legs;
+
+
   const map = useMap();//hook returns instance of a map and renders directions
   const routesLibrary = useMapsLibrary("routes"); //loads routes
   useEffect(()=>{
@@ -79,10 +86,13 @@ const PathfindingCard = () =>{
     }).then(res =>{
       directionsRenderer.setDirections(res);
       setRoutes(res.routes);
+
     }).catch((e:google.maps.MapsRequestError) =>{
       console.log("No such path exists!"); //will eventually be a toast ;()
     })
   }, [start, end, directionsService, directionsRenderer]);
+
+  useEffect(() => console.log(routes), [routes]);
 
 
   const handleSubmit = () =>{
@@ -94,11 +104,18 @@ const PathfindingCard = () =>{
 
   }
 
-
   return(
     <div className="pathfindingCardWrapper">
-      <Autocomplete onChange={(event, value) => setStartLocationTemp(value?value!.description:startLocationTemp)} filterOptions={(x)=>x} className="placeSelector" onInputChange={(event, value) => setValue(value)} renderInput={(params) => <TextField {...params} label="Origin"  onChange={(e) => setStartLocationTemp(e.target.value)}/>} options={data} getOptionLabel={(option) => typeof option === 'string'? option:option.description} />
-      <Autocomplete onChange={(event, value) => setEndLocationTemp(value?value!.description:startLocationTemp)} filterOptions={(x) => x} onInputChange={(event, value) => setValue(value)}  className = "placeSelector" renderInput={(params) => <TextField {...params} label="Destination" onChange={(e) => setEndLocationTemp(e.target.value)}/>} disablePortal options={data}  getOptionLabel={(option) => typeof option === 'string'? option:option.description}/>
+      <div className = "iconAutocompleteContainer">
+        <ShareLocationIcon sx={{fontSize:"4vh",  color:"darkblue"}}/>
+        <Autocomplete  onChange={(event, value) => setStartLocationTemp(value?value!.description:'')} filterOptions={(x)=>x} className="placeSelector" onInputChange={(event, value) => setValue(value)} renderInput={(params) => <TextField {...params} label="Origin"  onChange={(e) => setStartLocationTemp(e.target.value)}/>} options={data} getOptionLabel={(option) => typeof option === 'string'? option:option.description} />
+      </div>
+      <div className="iconAutocompleteContainer">
+        <FlagIcon sx={{fontSize:"4vh", color:"darkblue"}}/>
+        <Autocomplete onChange={(event, value) => setEndLocationTemp(value?value!.description:'')} filterOptions={(x) => x} onInputChange={(event, value) => setValue(value)}  className = "placeSelector" renderInput={(params) => <TextField {...params} label="Destination" onChange={(e) => setEndLocationTemp(e.target.value)}/>} disablePortal options={data}  getOptionLabel={(option) => typeof option === 'string'? option:option.description}/>
+
+      </div>
+
       <div className = "buttonContainer">
         <Button className = "pathfindingButton" variant="contained" sx={{backgroundColor:'darkblue'}} onClick={handleSubmit}>
           <div className = "buttonContent">
