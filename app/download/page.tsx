de5eb0dc61d69a4bbbf69ce9e134e14f8dab628e
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-30 16:48:36 -0400

    feat: protect /download page,  add auth0 prod keys, add logic for fetching trips

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -4,22 +4,31 @@ import {useUser} from "@auth0/nextjs-auth0";
 import axios from 'axios';
 import {Trip} from "@prisma/client";
 import './styles.css'
+import {useRouter} from "next/navigation";
 
 const Download = () => {
-  const {user} = useUser();
+  const router = useRouter();
+
+  const {user, isLoading} = useUser();
   const [trips, setTrips] = useState<Trip[]>([]);
+  const  [fetched, setFetched] = useState<boolean>(false);
 
   //UE to fetch user trips on load
+  const fetchData = async () => {
+    const trips = await axios.get(`/api/getTrips/${user!.nickname}`);
+    setTrips(trips.data.trips);
+    setFetched(true);
+  }
+  if(!user && !isLoading){
+    router.push('/auth/login')
+  }
+  else if (!fetched  &&  user && !isLoading){
+    fetchData().then();
+  }
 
-  useEffect(() =>{
-    const fetchData = async () => {
-      const trips = await axios.get(`/api/getTrips/${user!.nickname}`);
-      setTrips(trips.data);
-
-    }
+  useEffect(() => console.log(trips),  [trips])
 
 
-  })
   return (
     <div>
 
