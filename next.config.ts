62af1d44d4a190bd8983883171a0976f03a00601
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-30 13:37:35 -0400

    fix: expose auth0 env variables

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env:{
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+
+  },
   /* config options here */
   devIndicators:false
 };
