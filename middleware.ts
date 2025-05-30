bf1502dd10680f109b4402e539dcc3ce447a54d6
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-05-30 13:34:15 -0400

    fix: middleware function is no longer async

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import type { NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 
-export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+export  function middleware(request: NextRequest) {
+  return  auth0.middleware(request).then();
 }
 
 export const config = {
